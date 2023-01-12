4d31d242cb0cd5d59940b973718d244918bedfeb
jefferson-rod <jeffersonrodrigues.qa@gmail.com>
2023-01-12 11:40:25 -0300

    primeiro commit

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,7 +7,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.visit('src/index.html')
     })
 
-    it('verifica o título da aplicação', function () {
+   // it('verifica o título da aplicação', function () {
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
